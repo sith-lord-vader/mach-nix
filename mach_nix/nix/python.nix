33e4cb443c3604a580111c66215b845f2492b323
Abhishek Adhikari <abhishek.ad@media.net>
2024-07-25 16:18:57 +0530

    update python39 -> python311

--- mach_nix/nix/python.nix
+++ mach_nix/nix/python.nix
@@ -6,7 +6,7 @@
 }:
 let
   lib = pkgs.lib;
-  python = pkgs.python39;
+  python = pkgs.python311;
   pythonDeps = (lib.attrValues (import ./python-deps.nix { inherit python; fetchurl = pkgs.fetchurl; }));
   pythonDepsDev = with python.pkgs; [
     pytest_6
